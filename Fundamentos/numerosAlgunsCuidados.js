0597d90e88a7f7d163fa7aa34b5b2d03e1855bab
CaioGiacomelli <caio.giacomelli@hotmail.com>
2018-10-10 09:55:47 -0300

    Adicionado numeros.js e numerosAlgunsCuidados.js

---
+++ Fundamentos/numerosAlgunsCuidados.js
@@ -0,0 +1,6 @@
+console.log (7 / 0) // Tipo Infinity como retorno
+console.log('10' / 2) // Retorna 5
+console.log('Show' * 2) // Não funciona
+console.log(0.1 + 0.7) // Retorna 0.799999999999 
+// console.log(10.toString()) -> Não funciona e da exceção
+console.log((10).toString()); // Isso funciona
