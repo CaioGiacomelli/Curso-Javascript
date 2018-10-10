616dc7d3c7d7c8403384ffb1f830cc0162d688ce
CaioGiacomelli <caio.giacomelli@hotmail.com>
2018-10-10 09:05:20 -0300

    Adicionado comentario.js

---
+++ Fundamentos/comentario.js
@@ -0,0 +1,17 @@
+console.log('linha 1') // Comentário de uma linha
+
+/*
+    Comentário de
+    múltiplas linhas
+*/
+console.log('linha 2')
+
+
+/*
+ * Olá tudo
+ * Bom
+ * Com
+ * Vocês
+ * Galera
+ */
+console.log('linha 3')
