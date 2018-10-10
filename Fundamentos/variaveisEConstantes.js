fcceca73d0ac2e07359895de4af07003a323a6d5
CaioGiacomelli <caio.giacomelli@hotmail.com>
2018-10-10 09:23:51 -0300

    Adicionado variaveisEConstantes

---
+++ Fundamentos/variaveisEConstantes.js
@@ -0,0 +1,18 @@
+// Duas maneiras de declarar variáveis
+var a = 3 
+let b = 4 
+
+var a = 30 // Permite re-declarar com a palavra reservada Var.
+b = 40
+
+console.log(a, b)
+
+a = 300
+b = 400
+
+console.log(a, b)
+
+// Declaração de constante
+const c = 5
+
+console.log(c)
