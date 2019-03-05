6da60659ecdda58163e81466ae8088df98032c14
CaioGiacomelli <caio.giacomelli@hotmail.com>
2019-03-05 18:52:03 -0300

    Adicionado novos arquivos

---
+++ Objeto/heranca1.js
@@ -0,0 +1,23 @@
+// Todo objeto em JS por padrão tem um atributo chamado __proto__
+// Este atributo aponta para Object.prototype, o paizão de todos os objetos
+
+
+const ferrari = {
+    modelo: 'F40',
+    velMax: 324
+}
+
+const volvo = {
+    modelo: 'V40',
+    velMax: 200
+}
+
+console.log(ferrari.prototype)
+console.log(ferrari.__proto__)
+console.log(ferrari.__proto__ === Object.prototype)
+console.log(volvo.__proto__ === Object.prototype)
+console.log(Object.prototype.__proto__ === null)
+
+function MeuObjeto() {}
+console.log(typeof Object, typeof MeuObjeto)
+console.log(Object.prototype, MeuObjeto.prototype)
